69e961aff899b3696a047f3d54509acc9ccfbb9b
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-05-02 12:12:03 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter } from "react-router-dom"
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <div className="App">
+                
+            </div>
+        </BrowserRouter>
+    )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./src/**/*.{ts,tsx}"],
+    mode: "jit",
+    theme: {
+        extend: {
+
+        },
+    },
+    plugins: [],
+    darkMode: "class",
+}
+
