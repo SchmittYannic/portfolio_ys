9fb0f8bb5b82e4846cac27179847ad52988a3825
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-05-08 22:54:00 +0200

    Added Tilt to ProjectCard
    Added Tilt from react-parallax-tilt to ProjectCard.

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,4 +1,6 @@
 import { ReactElement, useContext, useState, useEffect, useRef } from "react"
+import Tilt from "react-parallax-tilt"
+
 import { SettingsContext } from "../context/SettingsProvider";
 import { ProjectType, TagKeyType, TextContent, projects, tagData } from "../constants";
 import { githubLogo, playButton } from "../assets";
@@ -65,63 +67,70 @@ const ProjectCard = ({ project }: {project: ProjectType}): ReactElement => {
     const description = lang === "de" ? project.description_de : project.description_en;
 
     return (
-        <div className="projectcard lg:max-w-none max-w-[410px] relative rounded-xl dark:bg-darkBaseTertiary/10 bg-baseTertiary/10  shadow-lg before:absolute before:top-0 before:left-0 before:h-full before:w-full before:rounded-xl before:content-[''] before:z-30 before:opacity-0 before:transition-opacity before:duration-500 before:hover:opacity-100 after:absolute after:top-0 after:left-0 after:h-full after:w-full after:rounded-xl after:content-[''] after:z-10 after:opacity-0 after:transition-opacity after:duration-500 group-hover:after:opacity-100">
-            {/* card background div. z-Index needs to be between ::before and ::after of parent */}
-            <div className="inset-[1px] absolute z-20 rounded-xl dark:bg-darkBaseTertiary bg-baseTertiary" />
-            
-            {/* project image and title container. z-Index needs to be higher than all for links to be clickable */}
-            <div className="px-10 pt-10 relative z-40">
-                <div className="relative">
-                    <img 
-                        className="rounded-xl w-full"
-                        src={project.image} 
-                        alt={`${lang === "de" 
-                            ? `Bild des Projects ${project.title_de}` 
-                            : `Image of the project ${project.title_en}`}`}
-                    />
+        <Tilt
+            scale={1}
+            transitionSpeed={250}
+            tiltMaxAngleX={5}
+            tiltMaxAngleY={5}  
+        >
+            <div className="projectcard lg:max-w-none max-w-[410px] relative rounded-xl dark:bg-darkBaseTertiary/10 bg-baseTertiary/10  shadow-lg before:absolute before:top-0 before:left-0 before:h-full before:w-full before:rounded-xl before:content-[''] before:z-30 before:opacity-0 before:transition-opacity before:duration-500 before:hover:opacity-100 after:absolute after:top-0 after:left-0 after:h-full after:w-full after:rounded-xl after:content-[''] after:z-10 after:opacity-0 after:transition-opacity after:duration-500 group-hover:after:opacity-100">
+                {/* card background div. z-Index needs to be between ::before and ::after of parent */}
+                <div className="inset-[1px] absolute z-20 rounded-xl dark:bg-darkBaseTertiary bg-baseTertiary" />
+                
+                {/* project image and title container. z-Index needs to be higher than all for links to be clickable */}
+                <div className="px-10 pt-10 relative z-40">
+                    <div className="relative">
+                        <img 
+                            className="rounded-xl w-full"
+                            src={project.image} 
+                            alt={`${lang === "de" 
+                                ? `Bild des Projects ${project.title_de}` 
+                                : `Image of the project ${project.title_en}`}`}
+                        />
 
-                    {project.liveLink &&
+                        {project.liveLink &&
+                            <a 
+                                className="group outline-none"
+                                href={project.liveLink} 
+                                target="_blank"
+                            >
+                                <div 
+                                    className="absolute top-4 right-20 w-12 h-12 rounded-full flex items-center justify-center bg-red-700 hover:scale-125 group-focus:ring-4 ring-red-700/30 ring-offset-2"
+                                >
+                                    <img 
+                                        className="w-5 h-5 translate-x-0.5"
+                                        src={playButton} 
+                                        alt="Live version" 
+                                    />
+                                </div>
+                            </a>
+                        }
+
                         <a 
                             className="group outline-none"
-                            href={project.liveLink} 
+                            href={project.githubLink} 
                             target="_blank"
                         >
-                            <div 
-                                className="absolute top-4 right-20 w-12 h-12 rounded-full flex items-center justify-center bg-red-700 hover:scale-125 group-focus:ring-4 ring-red-700/30 ring-offset-2"
-                            >
-                                <img 
-                                    className="w-5 h-5 translate-x-0.5"
-                                    src={playButton} 
-                                    alt="Live version" 
-                                />
-                            </div>
+                            <img 
+                                className="absolute top-4 right-4 w-12 h-12 rounded-full hover:scale-125 group-focus:ring-4 ring-black/30 ring-offset-2"
+                                src={githubLogo} 
+                                alt="Github Logo"
+                            />
                         </a>
-                    }
+                    </div>
 
-                    <a 
-                        className="group outline-none"
-                        href={project.githubLink} 
-                        target="_blank"
-                    >
-                        <img 
-                            className="absolute top-4 right-4 w-12 h-12 rounded-full hover:scale-125 group-focus:ring-4 ring-black/30 ring-offset-2"
-                            src={githubLogo} 
-                            alt="Github Logo"
-                        />
-                    </a>
+                    <h4 className="project-header my-10 text-2xl text-center dark:text-darkTextPrimary text-textPrimary">
+                        {lang === "de" ? project.title_de : project.title_en}
+                    </h4>
                 </div>
 
-                <h4 className="my-10 text-2xl text-center dark:text-darkTextPrimary text-textPrimary">
-                    {lang === "de" ? project.title_de : project.title_en}
-                </h4>
-            </div>
+                <ProjectDescription text={description} />
 
-            <ProjectDescription text={description} />
-
-            <div className="px-10 pb-10 relative z-40">
-                <Tags tags={project.tags} />
+                <div className="tags px-10 pb-10 relative z-40">
+                    <Tags tags={project.tags} />
+                </div>
             </div>
-        </div>
+        </Tilt>
     )
 }
 
@@ -153,7 +162,7 @@ const Projects = () => {
             <h2 className="mt-8 mb-16 text-5xl text-center dark:text-darkTextPrimary text-textPrimary">
                 {lang === "de" ? TextContent.german.projectsHead : TextContent.english.projectsHead}
             </h2>
-
+            
             <div className="lg:px-20 px-2 grid xl:grid-cols-3 lg:grid-cols-2 grid-cols-1 justify-items-center gap-4">
                 {projects.map((project, idx) => (
                     <ProjectCard
