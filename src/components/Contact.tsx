d25d738825614afec3d7d27738ce33f41da762f3
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-05-10 20:15:36 +0200

    Added loading circle for ContactForm
    Added a loading circle to the Sending button of the ContactForm to indicate the message is in the process of being send.

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -121,9 +121,15 @@ const ContactForm = (): ReactElement => {
                 </label>
 
                 <button
-                    className="py-3 px-8 rounded-md dark:bg-darkBase bg-base"
+                    className="py-3 px-8 rounded-md flex flex-row justify-center items-center dark:bg-darkBase bg-base"
                     type="submit"
                 >
+                    {isSending &&
+                        <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                        </svg>
+                    }
                     {lang === "de" ? TextContent.german.sendButton : TextContent.english.sendButton}
                 </button>
             </form>
