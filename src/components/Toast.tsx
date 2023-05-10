febb53a7071b494c95a7c0ba2ef14f3193ff3608
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-05-10 23:16:27 +0200

    Styling Toast
    Changing Toast background color. Fixing import in Toast Component.
    
    Comment out console.log in Contact Component.

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useContext } from "react";
-import { ToastType } from "../App";
+import { ToastType } from "../context/ToastProvider";
 import { closeBlack, closeWhite } from "../assets";
 import { SettingsContext } from "../context/SettingsProvider";
 
@@ -27,7 +27,7 @@ const Toast = ({ toastList }: {toastList: ToastType[]}) => {
                 list.map((toast, i) =>
                     <div
                         key={i}
-                        className="flex items-center w-full p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+                        className="flex items-center w-full p-4 mb-4 text-gray-500 bg-baseTertiary rounded-lg shadow dark:text-gray-400 dark:bg-darkBaseSecondary"
                         role="alert"
                     >
                         <div 
@@ -45,7 +45,7 @@ const Toast = ({ toastList }: {toastList: ToastType[]}) => {
                         <div className="ml-3 text-sm font-normal">{toast.text}</div>
                         <button
                             type="button"
-                            className="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" 
+                            className="ml-auto -mx-1.5 -my-1.5 bg-baseTertiary text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-base inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-darkBaseSecondary dark:hover:bg-darkBaseTertiary" 
                             data-dismiss-target={toast.type === "success" ? "#toast-success" : "#toast-failure"} 
                             aria-label="Close"
                             onClick={() => deleteToast(toast.id)}
