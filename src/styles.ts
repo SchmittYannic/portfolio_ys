89dcc1ffe04455cad9dcc8fac3be9a2d554aa50e
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-03-04 18:54:54 +0100

    Updates responsive styles

--- src/styles.ts
+++ src/styles.ts
@@ -2,16 +2,22 @@ const styles = {
     heroImgSection:
         "lg:max-w-[50%] lg:min-h-[100%] md:min-h-[500px] sm:min-h-[400px] min-h-[300px]",
     heroImg:
-        "lg:w-full lg:max-w-none sm:max-w-[300px] max-w-[215px]",
+        "lg:w-[700px] lg:h-[700px]",
+        // "lg:w-full lg:max-w-none sm:max-w-[300px] max-w-[215px]",
+
+    heroImgWidth: 
+        "3xl:w-[680px] 2xl:w-[500px] xl:w-[450px] sm:w-[300px] w-[250px]",
+    heroImgHeight:
+        "3xl:h-[680px] 2xl:h-[500px] xl:h-[450px] sm:h-[300px] w-[250px]",
 
     heroTextSection: 
-        "xl:max-w-[700px] lg:max-w-[550px] md:max-w-[500px] sm:max-w-[450px] max-w-[360px]",
+        "3xl:max-w-[700px] 2xl:max-w-[600px] lg:max-w-[500px] md:max-w-[500px] sm:max-w-[400px] max-w-[360px]",
     heroTextSectionPadding:
-        "lg:py-0 md:py-24 sm:py-16 py-10 lg:mx-auto lg:pl-20 px-10",
+        "lg:py-0 md:py-24 sm:py-16 py-10 lg:mx-auto lg:pl-20 lg:pr-0 px-10",
     heroHeadText:
-        "xl:text-7xl md:text-5xl sm:text-4xl text-3xl font-bold",
+        "3xl:text-7xl 2xl:text-6xl md:text-5xl sm:text-4xl text-3xl font-bold",
     heroSubText:
-        "xl:text-[25px] md:text-[20px] sm:text-[18px]",
+        "3xl:text-[25px] 2xl:text-[22px] md:text-[20px] sm:text-[18px]",
 }
 
 export { styles }
