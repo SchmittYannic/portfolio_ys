3158d5344cebc3cb83841401ef680a7277744b0a
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-03-05 13:54:20 +0100

    Updates Projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -27,15 +27,18 @@ const Projects = () => {
     const { lang, color } = useContext<UseSettingsContextType>(SettingsContext);
     const [activeProject, setActiveProject] = useState<ProjectType>(projects[0]);
 
+    const description = lang === "en" ? activeProject.description_en : activeProject.description_de;
+
     const bgColorClass900 = `bg-action${color}-900`;
     const borderColorClass200 = `border-action${color}-200`;
 
     return (
         <section id="projects" className="relative">
-            <div className="projects-background absolute inset-0">
-                <div className={`w-full h-[200px] ${bgColorClass900}`}></div>
+            <div className="projects-background absolute inset-0 flex flex-col justify-between">
+                <div className={`w-full h-[200px] ${bgColorClass900}`} />
+                <div className={`w-full h-[10px] ${bgColorClass900}`} />
             </div>
-            <div className="projects-content relative max-w-[1920px] min-w-[320px] mx-auto lg:px-48">
+            <div className="projects-content relative max-w-[1920px] min-w-[320px] mx-auto lg:px-48 pb-8">
                 <h2 className="pt-12 mb-12 text-5xl text-center dark:text-darkTextPrimary text-textPrimary">
                     {lang === "de" ? TextContent.german.projectsHead : TextContent.english.projectsHead}
                 </h2>
@@ -46,12 +49,21 @@ const Projects = () => {
                             <img src={activeProject.image} alt="" className={`w-[1024px] h-[576px] border-2 ${borderColorClass200}`} />
                         </div>
                         <div className="project-description">
-                            <h3 className="mt-8 text-4xl dark:text-darkTextPrimary text-textPrimary">
+                            <h3 className="mt-8 mb-4 text-4xl dark:text-darkTextPrimary text-textPrimary">
                                 {lang === "en" ? activeProject.title_en : activeProject.title_de}
                             </h3>
                         
-                            <div className="dark:text-darkTextPrimary text-textPrimary">
-                                {activeProject.description_de}
+                            <div className="mb-4 dark:text-darkTextPrimary text-textPrimary">
+                                {description.map((paragraph, idx) => 
+                                    <>
+                                        <p 
+                                            key={activeProject.title_en + idx}
+                                            className="mb-2 text-lg dark:text-darkTextPrimary text-textPrimary"
+                                        >
+                                            {paragraph}
+                                        </p>
+                                    </>
+                                )}
                             </div>
 
                             <Tags tags={activeProject.tags} />
@@ -66,8 +78,8 @@ const Projects = () => {
                                     type="button"
                                     onClick={() => setActiveProject(project)}
                                 >
-                                    <img src={project.image} alt="" className="w-[200px] h-[100px]" />
-                                    <h4 className="p-4 text-2xl grow dark:text-darkTextPrimary text-textPrimary">
+                                    <img src={project.image} alt="" className={`w-[180px] h-[100px] border-2 ${borderColorClass200}`} />
+                                    <h4 className="p-4 text-xl grow dark:text-darkTextPrimary text-textPrimary">
                                         {lang === "en" ? project.title_en : project.title_de}
                                     </h4>
                                 </button>
