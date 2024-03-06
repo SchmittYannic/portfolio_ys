2ab8da2e333def1d39af9ae10c32d192dd065124
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-03-06 21:03:50 +0100

    Deletes example project

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -348,16 +348,6 @@ const projects: ProjectType[] = [
         tags: ["React", "Typescript", "Tailwind"],
         githubLink: "https://github.com/SchmittYannic/portfolio_ys",
     },
-    {
-        title_de: "Elden Ring Buildplanner2",
-        title_en: "Elden Ring Buildplanner2",
-        image: placeholderProject,
-        description_de: ["No more than lipsum and some Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lo"],
-        description_en: ["No more than lipsum and some Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lo"],
-        tags: ["React", "Typescript", "SCSS"],
-        githubLink: "https://www.google.com",
-        liveLink: "https://www.google.com",
-    },
 ]
 
 export {
