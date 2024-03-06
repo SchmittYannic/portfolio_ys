67559fcfe4ebfee7f3b92aa36fc9ea7075e504ed
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-03-06 19:14:40 +0100

    Adds TechTags Component

---
+++ src/components/ui/TechTags.tsx
@@ -0,0 +1,23 @@
+import { TagKeyType, tagData } from "../../constants"
+
+const TechTags = ({ tags }: {tags: TagKeyType[]}) => {
+    return(
+        <div className="flex flex-wrap gap-2">
+            {tags.map((tag, idx) => {
+                const tagTextColor = tagData[tag].textColor
+                const tagBgColor = tagData[tag].backgroundColor
+
+                return (
+                    <div 
+                        key={idx}
+                        className={`px-2 rounded-sm ${tagTextColor} ${tagBgColor}`}
+                    >
+                        # {tag}
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default TechTags
