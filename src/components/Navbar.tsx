017e4833bfd4b61f344e74e16f924159c0c69948
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-03-07 20:39:08 +0100

    Adds margin top to nav and settings menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,7 @@ const NavMenu = ({ setIsMenuOpen, isSettingOpen, setIsSettingOpen }: NavMenuProp
     const cog: string = theme === "light" ? cogBlack : cogWhite;
 
     return (
-        <div className={`xl:ml-6 xl:mt-6 rounded-lg dark:bg-darkBase bg-base border-2 dark:border-darkBaseSecondary border-baseSecondary dark:shadow-darkTextPrimary/10 dark:shadow-md shadow-xl z-10`}>
+        <div className={`xl:ml-6 xl:mt-6 mt-1 rounded-lg dark:bg-darkBase bg-base border-2 dark:border-darkBaseSecondary border-baseSecondary dark:shadow-darkTextPrimary/10 dark:shadow-md shadow-xl z-10`}>
             <div className="px-4 py-3 text-sm text-textPrimary dark:text-darkTextPrimary">
                 <div className="text-center font-medium truncate">
                     {lang === "de" ? TextContent.german.navigation : TextContent.english.navigation}
@@ -159,7 +159,7 @@ const SettingsMenu = () => {
     }, [focusedRadio])
 
     return (
-        <div className="lg:mr-6 lg:mt-6 rounded-lg dark:bg-darkBase bg-base border-2 dark:border-darkBaseSecondary border-baseSecondary dark:shadow-darkTextPrimary/10 dark:shadow-md shadow-xl z-10">
+        <div className="lg:mr-6 lg:mt-6 mt-1 rounded-lg dark:bg-darkBase bg-base border-2 dark:border-darkBaseSecondary border-baseSecondary dark:shadow-darkTextPrimary/10 dark:shadow-md shadow-xl z-10">
             <div className="h-12 w-full px-5 py-2 flex items-center text-sm dark:text-darkTextPrimary text-textPrimary">
                 {lang === "de" ? TextContent.german.colorscheme : TextContent.english.colorscheme}
             </div>
