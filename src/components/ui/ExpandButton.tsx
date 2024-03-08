ac581ba9b44f29f12d98f5cd9b3d66bad5a22038
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-03-08 17:18:00 +0100

    Adds hover style to ExpandButton

--- src/components/ui/ExpandButton.tsx
+++ src/components/ui/ExpandButton.tsx
@@ -4,7 +4,7 @@ import { styles } from "../../styles"
 const ExpandButton = ({ children, ...rest }: PropsWithChildren<Omit<ButtonHTMLAttributes<HTMLButtonElement>, "className">>) => {
     return (
         <button 
-            className={`px-4 py-2 w-[130px] rounded-full ${styles.secondaryBackground} dark:text-darkTextPrimary text-textPrimary`}
+            className={`px-4 py-2 w-[130px] rounded-full ${styles.secondaryBackground} ${styles.secondaryHoverBackground} dark:text-darkTextPrimary text-textPrimary`}
             {...rest}
         >
             {children}
