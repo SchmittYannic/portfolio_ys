71e2b0941d1b010bebe4c06475b5a7bd0aa66b28
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-28 18:49:19 +0200

    Updates ExpandButton styles

--- src/components/ui/ExpandButton.tsx
+++ src/components/ui/ExpandButton.tsx
@@ -4,7 +4,7 @@ import { styles } from "../../styles"
 const ExpandButton = ({ children, ...rest }: PropsWithChildren<Omit<ButtonHTMLAttributes<HTMLButtonElement>, "className">>) => {
     return (
         <button
-            className={`px-4 py-2 w-[130px] rounded-full ${styles.primaryBackground} ${styles.primaryHoverBackground} dark:text-darkTextPrimary text-textPrimary`}
+            className={`px-4 py-2 w-[130px] rounded-full ${styles.primaryBackground} ${styles.primaryHoverBackground} ${styles.headlineTextColor}`}
             {...rest}
         >
             {children}
