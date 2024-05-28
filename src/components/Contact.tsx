d2ffb86e2f63baeed0f186c043854759a1d76433
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-29 00:51:15 +0200

    Fixes prop names of svg for react

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -43,9 +43,9 @@ const Contact = () => {
                                 viewBox="0 0 24 24"
                                 fill="none"
                                 stroke="currentColor"
-                                stroke-width="2"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
                                 className={`w-5 h-5 ${styles.primaryTextColor}`}
                             >
                                 <polyline points="22 12 16 12 14 15 10 15 8 12 2 12"></polyline>
