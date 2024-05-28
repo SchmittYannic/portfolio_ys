505733f8a9d224a3ee5862381621dc38c58e0103
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-28 17:08:13 +0200

    Updates styles

--- src/styles.ts
+++ src/styles.ts
@@ -3,17 +3,18 @@ const styles = {
         "max-w-[1400px]",
     primaryBackground:
         "dark:bg-darkBase-900 bg-base-900",
+    primaryHoverBackground:
+        "hover:dark:bg-darkBase-700 hover:bg-base-700",
     primaryTextColor:
         "text-gray-500 dark:text-gray-400",
     headlineTextColor:
         "text-base-0 dark:text-base-800",
     primaryBorderColor:
         "dark:border-darkBase-700 border-base-700",
+    secondaryBackground:
+        "bg-gray-100 dark:bg-gray-800",
 }
 
 export {
     styles,
-    // heroStyles,
-    // projectsStyles,
-    // projectsMobileStyles,
 }
