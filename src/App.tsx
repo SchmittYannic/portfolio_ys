4fee587886ffd916405934be1062785882776116
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-29 17:35:47 +0200

    Removes comments from App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,7 @@ import Layout from "./components/Layout";
 import Portfolio from "./components/Portfolio";
 
 const App = () => {
-    // const windowSize = useWindowSize();
-
-    // const isXlScreen = windowSize.width && windowSize.width >= 1280;
-
     return (
-        // <BrowserRouter>
-        //     <div className={`app relative w-full h-full ${styles.primaryBackground}`}>
-        //         <Navbar />
-        //         <Hero />
-        //         {isXlScreen ? <Projects /> : <ProjectsMobile />}
-        //         <About />
-        //         <Contact />
-        //         <Toast toastList={toastList} />
-        //     </div>
-        // </BrowserRouter>
         <Routes>
             <Route path="/" element={<Layout />} >
                 <Route index element={<Portfolio />} />
