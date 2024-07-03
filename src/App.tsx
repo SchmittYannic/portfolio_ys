7241c8d501e6e18da9b163eb1d9b5e899a4c6161
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-07-03 19:23:57 +0200

    Adds route to Impressum to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/Layout";
 import Portfolio from "./components/Portfolio";
+import Impressum from "./components/Impressum";
 
 const App = () => {
     return (
@@ -8,6 +9,8 @@ const App = () => {
             <Route path="/" element={<Layout />} >
                 <Route index element={<Portfolio />} />
             </Route>
+
+            <Route path="/impressum" element={<Impressum />} />
 
             {/* if route doesnt exist redirect back to frontpage */}
             <Route path="*" element={<Navigate to="/" />} />
