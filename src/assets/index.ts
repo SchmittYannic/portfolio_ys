bbff89532f5e014391aecb6d91049dfe517dccbd
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2025-03-21 17:20:24 +0100

    Deletes unused assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -10,8 +10,6 @@ import cogBlack from "./cogBlack.svg";
 import cogWhite from "./cogWhite.svg";
 import logoBlack from "./logoBlack.svg";
 import logoWhite from "./logoWhite.svg";
-import menuBlack from "./menuBlack.svg";
-import menuWhite from "./menuWhite.svg";
 import uk from "./uk.svg";
 import germany from "./germany.svg";
 import checkmarkWhite from "./checkmarkWhite.svg";
@@ -21,11 +19,8 @@ import calendarBlack from "./calendarBlack.svg";
 import html5Logo from "./html5.svg";
 import css3Logo from "./css3.svg";
 import javascriptLogo from "./javascript.svg";
-import reactLogo from "./react.svg";
-import pythonLogo from "./python.svg";
 import typescriptLogo from "./typescript.svg";
 
-import githubLogo from "./github.svg";
 import playButton from "./playButton.svg";
 import hairdresserAppimg from "./hairdresserAppimg.webp"
 import hairdresserApp from "./hairdresserApp.webm";
@@ -48,8 +43,6 @@ export {
     cogWhite,
     logoBlack,
     logoWhite,
-    menuBlack, //currently not used
-    menuWhite, //currently not used
     uk,
     germany,
     checkmarkBlack,
@@ -58,10 +51,7 @@ export {
     html5Logo,
     css3Logo,
     javascriptLogo,
-    reactLogo, //currently not used
-    pythonLogo, //currently not used
     typescriptLogo,
-    githubLogo, //currently not used
     playButton,
     hairdresserAppimg,
     hairdresserApp,
