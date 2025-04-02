02f03bdff72142abf213896ee618a2111360e2c1
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2025-04-02 11:31:54 +0200

    Moves localStorage related code from Layout component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,48 @@
+import { useEffect } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+import useSettings from "./hooks/useSettings";
 import Layout from "./components/Layout";
 import Portfolio from "./components/Portfolio";
 import Impressum from "./components/Impressum";
 
 const App = () => {
+
+    const { setTheme, setLang, setColor } = useSettings();
+
+    useEffect(() => {
+        // if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        //     document.documentElement.classList.add('dark')
+        //     setTheme("dark")
+        // } else {
+        //     document.documentElement.classList.remove('dark')
+        //     setTheme("light")
+        // }
+
+        if (localStorage.theme === 'light') {
+            document.documentElement.classList.remove('dark')
+            setTheme("light")
+        } else {
+            document.documentElement.classList.add('dark')
+            setTheme("dark")
+        }
+
+        if (localStorage.lang === 'en') {
+            setLang("en")
+            document.documentElement.setAttribute("lang", "en")
+        } else {
+            setLang("de")
+            document.documentElement.setAttribute("lang", "de")
+        }
+
+        if (localStorage.color) {
+            setColor(localStorage.color);
+            document.documentElement.setAttribute("data-color", localStorage.color);
+        } else {
+            setColor("Blue");
+            document.documentElement.setAttribute("data-color", "Blue");
+        }
+    }, []);
+
     return (
         <Routes>
             <Route path="/" element={<Layout />} >
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,47 +1,9 @@
-import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
-import useSettings from "../hooks/useSettings";
 import Navbar from "./Navbar"
 import Footer from "./Footer";
 import { styles } from "../styles";
 
 const Layout = () => {
-
-    const { setTheme, setLang, setColor } = useSettings();
-
-    useEffect(() => {
-        // if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-        //     document.documentElement.classList.add('dark')
-        //     setTheme("dark")
-        // } else {
-        //     document.documentElement.classList.remove('dark')
-        //     setTheme("light")
-        // }
-
-        if (localStorage.theme === 'light') {
-            document.documentElement.classList.remove('dark')
-            setTheme("light")
-        } else {
-            document.documentElement.classList.add('dark')
-            setTheme("dark")
-        }
-
-        if (localStorage.lang === 'en') {
-            setLang("en")
-            document.documentElement.setAttribute("lang", "en")
-        } else {
-            setLang("de")
-            document.documentElement.setAttribute("lang", "de")
-        }
-
-        if (localStorage.color) {
-            setColor(localStorage.color);
-            document.documentElement.setAttribute("data-color", localStorage.color);
-        } else {
-            setColor("Blue");
-            document.documentElement.setAttribute("data-color", "Blue");
-        }
-    }, []);
 
     return (
         <div
