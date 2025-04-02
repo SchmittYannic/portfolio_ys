4198991da8edddd793c23b28363e48faaea3034d
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2025-04-02 12:43:50 +0200

    Update Impressum.tsx

--- src/components/Impressum.tsx
+++ src/components/Impressum.tsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom"
+import useSettings from "../hooks/useSettings";
 import { styles } from "../styles"
 import { Navbar } from ".";
 
 const Impressum = () => {
+
+    const { lang } = useSettings();
 
     const firstname = String(import.meta.env.VITE_FIRSTNAME) ?? "";
     const lastname = String(import.meta.env.VITE_LASTNAME) ?? "";
@@ -9,6 +13,8 @@ const Impressum = () => {
     const plz = String(import.meta.env.VITE_PERSONAL_PLZ) ?? "";
     const city = String(import.meta.env.VITE_PERSONAL_CITY) ?? "";
     const email = String(import.meta.env.VITE_PERSONAL_EMAIL) ?? "";
+
+    const buttonText = lang === "de" ? "Zurück zur Startseite" : "Back to homepage";
 
     return (
         <main
@@ -23,7 +29,7 @@ const Impressum = () => {
                     >
                         Impressum
                     </h1>
-                    <div className="mt-8">
+                    <div className="mt-8 mb-6">
                         <p className={`${styles.primaryFontSize} ${styles.primaryTextColor}`}>
                             {firstname} {lastname}
                         </p>
@@ -46,6 +52,15 @@ const Impressum = () => {
                             </a>
                         </p>
                     </div>
+                    <Link
+                        className={`px-4 py-2 rounded-full ${styles.secondaryBackground} ${styles.primaryHoverBackground} ${styles.headlineTextColor} inline-flex items-center`}
+                        to="/"
+                    >
+                        <svg className="mr-2" width="12" height="12" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M768 903.232l-50.432 56.768L256 512l461.568-448 50.432 56.768L364.928 512z" fill="currentColor" />
+                        </svg>
+                        <span className="h-5 leading-none">{buttonText}</span>
+                    </Link>
                 </div>
             </div>
         </main>
