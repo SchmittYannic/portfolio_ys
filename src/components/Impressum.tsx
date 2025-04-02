8912c70ba56a0750d253990ee0f2fcbe7d2c43e2
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2025-04-02 12:46:45 +0200

    Update Impressum.tsx

--- src/components/Impressum.tsx
+++ src/components/Impressum.tsx
@@ -59,7 +59,7 @@ const Impressum = () => {
                         <svg className="mr-2" width="12" height="12" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">
                             <path d="M768 903.232l-50.432 56.768L256 512l461.568-448 50.432 56.768L364.928 512z" fill="currentColor" />
                         </svg>
-                        <span className="h-5 leading-none">{buttonText}</span>
+                        <span>{buttonText}</span>
                     </Link>
                 </div>
             </div>
