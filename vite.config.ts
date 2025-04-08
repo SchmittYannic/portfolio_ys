40e55ec0266fc9e76bb0afcb311f4ea91c21e4c2
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2025-04-08 20:24:39 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,13 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-    const env = loadEnv(mode, process.cwd(), '');
-    return {
-        define: {
-            'process.env.EMAILJS_PUBLIC_KEY': JSON.stringify(env.EMAILJS_PUBLIC_KEY),
-            'process.env.EMAILJS_TEMPLATE_ID': JSON.stringify(env.EMAILJS_TEMPLATE_ID),
-            'process.env.EMAILJS_SERVICE_ID': JSON.stringify(env.EMAILJS_SERVICE_ID),
-        },
-        plugins: [
-            react(),
-        ],
-    };
-});
-  
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            src: "/src",
+        }
+    }
+})
+
